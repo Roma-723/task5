9bbf1d15a0565a93c5a32b54fa8248df81652711
Roma-723 <romajon.ll8@gmail.com>
2026-05-29 11:46:49 +0500

    nmd

--- src/companent/filtermaps.tsx
+++ src/companent/filtermaps.tsx
@@ -479,8 +479,6 @@ function FilterItem({
 
 <p>dwevefqwqdefvre</p>
 <p>dwevefqwqdefvre</p>
-<p>dwevefqwqdefvre</p>
-<p>dwevefqwqdefvre</p>
 
               {filter.conditions[0]?.values.map(
                 (
