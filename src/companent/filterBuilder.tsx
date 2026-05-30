2e9193a377f772f1bf99fcd2f5d8cdae9978a03a
Roma-723 <romajon.ll8@gmail.com>
2026-05-30 09:14:47 +0500

    name

--- src/companent/filterBuilder.tsx
+++ src/companent/filterBuilder.tsx
@@ -843,7 +843,6 @@ export default function FilterBuilder() {
             <p>sfvsd</p>
             <p>sfvsd</p>
             <p>sfvsd</p>
-            <p>sfvsd</p>
           </div>
         </div>
       </div>
