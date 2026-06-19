635975114215a7f445cf991a134cacf287cae344
Roma-723 <romajon.ll8@gmail.com>
2026-06-19 17:54:56 +0500

    firsName

--- src/companent/filterBuilder.tsx
+++ src/companent/filterBuilder.tsx
@@ -813,9 +813,10 @@ export default function FilterBuilder() {
                 Добавить команду
               </Button>
 
+
               <Button
                 size="large"
-                
+
                 className="h-12 px-10 rounded-[14px]"
                 onClick={clearFilters}
               >
